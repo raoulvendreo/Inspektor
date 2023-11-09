0e3463aba58c3429f968c2792106cd2001403f31
Raoul.Achdiyat <jaerith@gmail.com>
2023-11-09 10:53:45 +0700

    Inspektor App

---
+++ app/src/main/java/com/example/inspektor/VehicleDashboardActivity.java
@@ -0,0 +1,14 @@
+package com.example.inspektor;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class VehicleDashboardActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_vehicle_dashboard);
+    }
+}
